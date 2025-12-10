954dbeb1bb1c7b53797a6d145159a776146e9647
Dorian <64016619+DorianGrey11@users.noreply.github.com>
2025-12-10 17:00:06 +0100

    feat: disable prefetch

--- astro.config.ts
+++ astro.config.ts
@@ -1,18 +1,19 @@
-import { defineConfig } from 'astro/config';
+import {defineConfig} from 'astro/config';
 import sitemap from '@astrojs/sitemap';
 import UnoCSS from 'unocss/astro';
 
 export default defineConfig({
-  // used to generate images
-  site:'https://17.nzbka.de',
-  trailingSlash: 'ignore',
-  integrations: [sitemap(), UnoCSS({ injectReset: true })],
-  vite: {
-    optimizeDeps: {
-      exclude: ['@resvg/resvg-js'],
+    // used to generate images
+    site: 'https://17.nzbka.de',
+    trailingSlash: 'ignore',
+    integrations: [sitemap(), UnoCSS({injectReset: true})],
+    vite: {
+        optimizeDeps: {
+            exclude: ['@resvg/resvg-js'],
+        },
     },
-  },
-  redirects: {
-    "/question": "/question/1",
-  },
+    redirects: {
+        "/question": "/question/1",
+    },
+    prefetch: false,
 });
